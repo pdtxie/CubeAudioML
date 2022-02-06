4c3ea0b9b4a9189d556945281c7097544c39d5a3
Tim Xie <pdt.xie@gmail.com>
2022-02-06 20:30:12 +1300

    upload files

--- CubeAudioML/CubeAudioML/ContentView.swift
+++ CubeAudioML/CubeAudioML/ContentView.swift
@@ -1,21 +1,52 @@
-//
-//  ContentView.swift
-//  CubeAudioML
-//
-//  Created by Tim Xie on 6/02/22.
-//
-
 import SwiftUI
+import AVFoundation
+import SoundAnalysis
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+class Manager: NSObject, SNResultsObserving, ObservableObject {
+    func request(_ request: SNRequest, didProduce result: SNResult) {
+        NSLog("i have no idae what is going on: \(result)")
+    }
+    
+    private let audioEngine = AVAudioEngine()
+    private var soundClassifier = CubeSoundClassifier()
+    var streamAnalyzer: SNAudioStreamAnalyzer!
+    
+    private func startAudio() {
+        audioEngine.prepare()
+        try! audioEngine.start()
+    }
+    
+    private func prepare() {
+        let inputNode = audioEngine.inputNode
+        let recFmt = inputNode.outputFormat(forBus: 0)
+        streamAnalyzer = SNAudioStreamAnalyzer(format: recFmt)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recFmt) {
+            [unowned self] (buffer, when) in
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.streamAnalyzer.analyze(buffer, atAudioFramePosition: when.sampleTime)
+                NSLog("i think it is analyzing something")
+            }
+        }
+        startAudio()
+    }
+    
+    private func createReq() {
+        let req = try! SNClassifySoundRequest(mlModel: soundClassifier.model)
+        try! streamAnalyzer.add(req, withObserver: self)
+    }
+    
+    func start() {
+        prepare()
+        createReq()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    @StateObject var manager = Manager()
+    
+    var body: some View {
+        Button("start") {
+            manager.start()
+        }
     }
 }
