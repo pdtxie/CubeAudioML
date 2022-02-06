3c6c0bb609fc63b1eb740bb9b22bb7690fcc316f
Tim Xie <pdt.xie@gmail.com>
2022-02-06 20:28:44 +1300

    first working commit

---
+++ CubeAudioML/CubeAudioML/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CubeAudioML
+//
+//  Created by Tim Xie on 6/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CubeAudioML/CubeAudioML/CubeAudioMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubeAudioMLApp.swift
+//  CubeAudioML
+//
+//  Created by Tim Xie on 6/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CubeAudioMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
